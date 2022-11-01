602b3ed0d8070db406acc4a1b9e81a2cf2c149ac
Gustavo Santana Araujo <gustavo.consultor@jcl-tecnologia.com.br>
2022-11-01 12:23:58 -0300

    Inserting and removing products into categories

--- admin-categories.php
+++ admin-categories.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Hcode\Model\Category;
+use Hcode\Model\Product;
 use \Hcode\Model\User;
 use \Hcode\PageAdmin;
 
@@ -97,6 +98,65 @@ $app->post('/admin/categories/:idcategory', function($idcategory) {
 	$category->save();//since this method rely on the stored procedure that is fitted for both update and create category, we can use it
 
 	header("Location: /admin/categories");
+	exit;
+
+});
+
+//get products of a specific category and the rest of the products
+$app->get('/admin/categories/:idcategory/products', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-products", array(
+		"category"=>$category->getValues(),
+		"productsRelated"=>$category->getProducts(),
+		"productsNotRelated"=>$category->getProducts(false)
+	));
+
+});
+
+//adding a product into a category
+$app->get('/admin/categories/:idcategory/products/:idproduct/add', function($idcategory, $idproduct) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$category->addProduct($product);
+
+	header("Location: /admin/categories/" . $idcategory . "/products");
+	exit;
+
+});
+
+//removing a product of a category
+$app->get('/admin/categories/:idcategory/products/:idproduct/remove', function($idcategory, $idproduct) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$category->removeProduct($product);
+
+	header("Location: /admin/categories/" . $idcategory . "/products");
 	exit;
 
 });
--- site.php
+++ site.php
@@ -29,7 +29,7 @@ $app->get('/categories/:idcategory', function($idcategory) {
 
 	$page->setTpl("category", array(
 		"category"=>$category->getValues(),
-		'products'=>[]
+		'products'=>Product::checkList($category->getProducts())
 	));
 
 });
