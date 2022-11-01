b6c1ce57ddc0a2e6c9a5ae63a4bdc8dc04ec2de3
Gustavo Santana Araujo <gustavo.consultor@jcl-tecnologia.com.br>
2022-11-01 17:21:22 -0300

    Automatic pagination working

--- site.php
+++ site.php
@@ -20,16 +20,30 @@ $app->get('/', function() {//root folder, no parameters to routes involved
 
 //list itens on a category
 $app->get('/categories/:idcategory', function($idcategory) {
+
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 	
 	$category = new Category();
 
 	$category->get((int)$idcategory);
 
+	$pagination = $category->getProductsPage($page);
+
+	$pages = array();
+
+	for ($i=1; $i <= $pagination['pages']; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/' . $category->getidcategory() . '?page=' . $i,
+			'page'=>$i
+		]);
+	}
+
 	$page = new Page();
 
 	$page->setTpl("category", array(
 		"category"=>$category->getValues(),
-		'products'=>Product::checkList($category->getProducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	));
 
 });
