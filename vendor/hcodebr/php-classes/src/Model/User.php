6cbe1bbdb924228dd3a9defdfd6d4ca177a4fe05
Gustavo <sasaeucomvoce@hotmail.com>
2022-12-19 22:59:04 -0300

    New user register working

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -310,6 +310,37 @@ class User extends Model {
         return password_hash($password, PASSWORD_DEFAULT, ['cost'=>12]);
     }
 
+    public static function setErrorRegister($msg)
+    {
+        $_SESSION[User::ERROR_REGISTER] = $msg;
+    }
+
+    public static function getErrorRegister()
+    {
+        $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+        User::clearErrorRegister();
+
+        return $msg;
+        
+    }
+
+    public static function clearErrorRegister()
+    {
+        $_SESSION[User::ERROR_REGISTER] = NULL;
+    }
+
+    public static function checkLoginExists($login)
+    {
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+            ":deslogin"=>$login
+        ]);
+        
+        return (count($results) > 0);
+    }
+
 
 }
 
